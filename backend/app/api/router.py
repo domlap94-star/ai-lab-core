67bc8e6e57cf41d7c99968a6ddc54c59f1a89b2e
domlap94-star <domlap94@gmail.com>
2026-07-29 00:43:32 +0200

    Update router.py

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from app.api.ai import router as ai_router
 from app.api.auth import router as auth_router
 from app.api.users.router import router as users_router
 from app.core.constants import API_PREFIX
@@ -10,3 +11,4 @@ api_router = APIRouter(
 
 api_router.include_router(auth_router)
 api_router.include_router(users_router)
+api_router.include_router(ai_router)
